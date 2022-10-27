12658e98ec39dfbefe12345a717b7f61f98d3996
169dodocha <dodocha1524@icloud.com>
2022-10-27 14:43:40 +0900

    add settings.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_practice_profile_page/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_practice_profile_page/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
